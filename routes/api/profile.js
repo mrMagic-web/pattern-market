18dc6177360a5578135d3245ebe31401285cea55
mrMagic-web <maciejdrahusz@gmail.com>
2018-09-27 21:09:43 +0200

    User profile schema and route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,31 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const passport = require('passport');
+
 const router = express.Router();
 
-// @route     GET /api/posts
-// @desc      individual user profile
-// @access    Public
-router.get('/', (req, res) => res.json({ msg: "profiles works"}));
+// Load profile model
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+
+// @route     GET /api/profile
+// @desc      Get current users profile
+// @access    Private
+router.get('/',passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    const errors = {};
+
+    Profile.findOne({ user: req.user.id })
+      .populate('user', ['name', 'avatar'])
+      .then(profile => {
+        if (!profile) {
+          errors.noprofile = 'There is no profile for this user';
+          return res.status(404).json(errors);
+        }
+        res.json(profile);
+      })
+      .catch(err => res.status(404).json(err));
+  }
+);
 
 module.exports = router;
