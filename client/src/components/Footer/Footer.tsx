7002194a7c62cc4f89cc1c932b0b9cde95519259
mrMagic-web <maciejdrahusz@gmail.com>
2018-10-14 13:40:11 +0200

    Basic react shell

---
+++ client/src/components/Footer/Footer.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+class Footer extends React.Component<any, any>  {
+  public render() {
+    return (
+      <div>
+        Footer
+      </div>
+    );
+  }
+}
+
+export default Footer;
+  
