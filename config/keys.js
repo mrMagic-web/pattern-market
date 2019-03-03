498965f7ba5ae34ec88d42f8d70963524c9d32e8
mrMagic-web <maciejdrahusz@gmail.com>
2019-03-03 12:50:29 +0100

    Keys securing

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-  mongoURI: 'mongodb://maciek:drahaslo0@ds111623.mlab.com:11623/pattern-shop',
-  secretOrKey: 'secret'
+if (process.env.NODE_ENV === "production") {
+	module.exports = require("./keys_prod");
+} else {
+	module.exports = require("./keys_dev");
 }
