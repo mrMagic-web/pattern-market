a53b8c076f03b34339865fb8c23644def815518b
mrMagic-web <maciejdrahusz@gmail.com>
2019-03-17 11:20:58 +0100

    Prepared for deploy

--- client/ts-errors.js
+++
@@ -1,7 +0,0 @@
-//Type '{}' has no property 'email' and no string index signature. - dodac constructor
-constructor(props: any) {
-    super(props);
-}
-// Type '{}' is not assignable to type 'ITextFieldProps'. Property 'name' is missing in type '{}'.
-
-// Property '' does not exist on type
